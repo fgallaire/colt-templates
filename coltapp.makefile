5a8213e7665af103293318ee522ddc4870b7b0e4
Florent Gallaire <fgallaire@gmail.com>
2013-12-02 08:23:56 +0100

    Add the coltapp template.

---
+++ coltapp.makefile
@@ -0,0 +1,7 @@
+PROJECT = {{appid}}
+
+DEPS = cowboy erlydtl
+dep_cowboy = pkg://cowboy master
+dep_erlydtl = pkg://erlydtl 4d0dc8fb
+
+include erlang.mk
---
+++ coltapp_index_http_controller.erl
@@ -0,0 +1,18 @@
+%% @doc index HTTP controller.
+-module(index_http_controller).
+
+-export([init/3]).
+-export([handle/2]).
+-export([terminate/3]).
+
+init(_Type, Req, []) ->
+	{ok, Req, undefined}.
+
+handle(Req, State) ->
+	Headers = [{<<"content-type">>, <<"text/html">>}],
+	{ok, Content} = index_template:render([{title, "Your Colt is shooting !"}, {text, "Your Colt is shooting HTTP as HTML !"}]),
+	{ok, Req2} = cowboy_req:reply(200, Headers, Content, Req),
+	{ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+	ok.

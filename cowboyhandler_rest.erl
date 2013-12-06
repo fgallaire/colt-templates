18b68a13b89f28d610b4cec3aacdd03e74fe8446
Florent Gallaire <fgallaire@gmail.com>
2013-12-06 05:35:54 +0100

    Add handlerid to the rest handler.

--- cowboyhandler_rest.erl
+++ cowboyhandler_rest.erl
@@ -3,21 +3,21 @@
 
 -export([init/3]).
 -export([content_types_provided/2]).
--export([hello_to_html/2]).
--export([hello_to_json/2]).
--export([hello_to_text/2]).
+-export([{{handlerid}}_to_html/2]).
+-export([{{handlerid}}_to_json/2]).
+-export([{{handlerid}}_to_text/2]).
 
 init(_Transport, _Req, []) ->
 	{upgrade, protocol, cowboy_rest}.
 
 content_types_provided(Req, State) ->
 	{[
-		{<<"text/html">>, hello_to_html},
-		{<<"application/json">>, hello_to_json},
-		{<<"text/plain">>, hello_to_text}
+		{<<"text/html">>, {{handlerid}}_to_html},
+		{<<"application/json">>, {{handlerid}}_to_json},
+		{<<"text/plain">>, {{handlerid}}_to_text}
 	], Req, State}.
 
-hello_to_html(Req, State) ->
+{{handlerid}}_to_html(Req, State) ->
 	Body = <<"<html>
 <head>
 	<meta charset=\"utf-8\">
@@ -29,9 +29,9 @@ hello_to_html(Req, State) ->
 </html>">>,
 	{Body, Req, State}.
 
-hello_to_json(Req, State) ->
+{{handlerid}}_to_json(Req, State) ->
 	Body = <<"{\"rest\": \"Your Cowboy is shooting REST as JSON !\"}">>,
 	{Body, Req, State}.
 
-hello_to_text(Req, State) ->
+{{handlerid}}_to_text(Req, State) ->
 	{<<"Your Cowboy is shooting REST as text!">>, Req, State}.
